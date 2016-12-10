cb21a3c7ed3ce598e8d0e5b50b1d114c50258055
anupritaisno1 <www.anuprita804@gmail.com>
2016-12-10 15:50:31 +0000

    fix a big which caused the devoce to believe that it was a google pixel and NOT a google pixel xl; also made it more believable

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -3,8 +3,11 @@
 # This will make your scripts compatible even if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
-/data/magisk/resetprop ro.product.model Pixel XL
-/data/magisk/resetprop ro.opa.eligible_device true
-
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
+
+/data/magisk/resetprop -n ro.product.model "Pixel XL"
+/data/magisk/resetprop -n ro.opa.eligible_device true
+/data/magisk/resetprop -n ro.product.manufacturer Google
+
+# thanks to xda (at)Link_of_Hyrule,  xda (at)stangri
