61e9f8de0e4c305e1fc2cd31493b87d26735492d
anupritaisno1 <www.anuprita804@gmail.com>
2016-12-10 14:28:00 +0000

    y'all got any more of them pixels?

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -3,5 +3,8 @@
 # This will make your scripts compatible even if Magisk change its mount point in the future
 MODDIR=${0%/*}
 
+/data/magisk/resetprop ro.product.model Pixel XL
+/data/magisk/resetprop ro.opa.eligible_device true
+
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
--- config.sh
+++ config.sh
@@ -40,7 +40,7 @@ MODID=template
 AUTOMOUNT=true
 
 # Set to true if you need post-fs-data script
-POSTFSDATA=false
+POSTFSDATA=true
 
 # Set to true if you need late_start service script
 LATESTARTSERVICE=false
